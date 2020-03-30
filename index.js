ceb4268114fca1ae11f8d7392e371b4b1a37d413
Farhan Asif <f.asif2010@gmail.com>
2020-03-31 01:53:35 +0600

    variable

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const http = require('http');
 const app = require('./app');
-const port = process.env.PORT || 4000;
+const port = process.env.APP_PORT || 4000;
 
 const server = http.createServer(app);
 server.listen(port);
